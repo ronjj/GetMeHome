1b59fcd322d5054bb8adeb45d615628fe20883f6
Ronald Jabouin <14562+rj336@users.noreply.github.coecis.cornell.edu>
2023-11-23 22:21:32 -0500

    fixed type in sample response. made trip struct in ios app

--- frontend/GetMeHome/GetMeHome/ContentView.swift
+++ frontend/GetMeHome/GetMeHome/ContentView.swift
@@ -10,6 +10,12 @@ import SwiftUI
 struct Trip: Hashable {
     let date: String
     let price: Float
+    let arrival_time: String
+    let arrival_location: String
+    let departure_time: String
+    let departure_location: String
+    let bus_service: String
+    let non_stop: String
 }
 
 struct ContentView: View {
