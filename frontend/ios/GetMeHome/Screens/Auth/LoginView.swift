ec4dc681f54b788d7211c1ca11ea507273155828
Ronald Jabouin <ronaldjabouin2004@gmail.com>
2024-01-11 15:39:28 -0500

    removed email sign in fields and sign up button from login view

--- frontend/ios/GetMeHome/Screens/Auth/LoginView.swift
+++ frontend/ios/GetMeHome/Screens/Auth/LoginView.swift
@@ -46,64 +46,6 @@ struct LoginView: View {
                     .font(.title)
                     .foregroundStyle(.white)
                 
-                HStack {
-                    Image(systemName: "at")
-                    TextField("Email", text: $viewModel.email)
-                        .textInputAutocapitalization(.never)
-                        .disableAutocorrection(true)
-                        .focused($focus, equals: .email)
-                        .submitLabel(.next)
-                        .onSubmit {
-                            self.focus = .password
-                        }
-                }
-                .padding(.vertical, 6)
-                .background(Divider(), alignment: .bottom)
-                .padding(.bottom, 4)
-                
-                HStack {
-                    Image(systemName: "lock")
-                    SecureField("Password", text: $viewModel.password)
-                        .focused($focus, equals: .password)
-                        .submitLabel(.go)
-                        .onSubmit {
-                            signInWithEmailPassword()
-                        }
-                }
-                .padding(.vertical, 6)
-                .background(Divider(), alignment: .bottom)
-                .padding(.bottom, 8)
-                
-                if !viewModel.errorMessage.isEmpty {
-                    VStack {
-                        Text(viewModel.errorMessage)
-                            .foregroundColor(Color(UIColor.systemRed))
-                    }
-                }
-                
-                Button(action: signInWithEmailPassword) {
-                    if viewModel.authenticationState != .authenticating {
-                        Text("Login")
-                            .padding(.vertical, 8)
-                            .frame(maxWidth: .infinity)
-                    }
-                    else {
-                        ProgressView()
-                            .progressViewStyle(CircularProgressViewStyle(tint: .white))
-                            .padding(.vertical, 8)
-                            .frame(maxWidth: .infinity)
-                    }
-                }
-                .disabled(!viewModel.isValid)
-                .frame(maxWidth: .infinity)
-                .buttonStyle(.borderedProminent)
-                
-                HStack {
-                    VStack { Divider() }
-                    Text("or")
-                    VStack { Divider() }
-                }
-                
                 Button(action: signInWithGoogle) {
                     Text("Sign in with Google")
                         .padding(.vertical, 8)
@@ -115,16 +57,7 @@ struct LoginView: View {
                 }
                 .foregroundColor(colorScheme == .dark ? .white : .black)
                 .buttonStyle(.bordered)
-                
-                HStack {
-                    Text("Don't have an account yet?")
-                    Button(action: { viewModel.switchFlow() }) {
-                        Text("Sign up")
-                            .fontWeight(.semibold)
-                            .foregroundColor(.blue)
-                    }
-                }
-                .padding([.top, .bottom], 50)
+
                 
             }
             .listStyle(.plain)
