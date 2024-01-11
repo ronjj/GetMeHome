a1f9691fc7dbf240bb47d6c29d4127937b7ad1fc
Ronald Jabouin <ronaldjabouin2004@gmail.com>
2024-01-11 15:29:04 -0500

    removed authenticatedview. felt it was an unnecessary middle screen

--- frontend/ios/GetMeHome/GetMeHomeApp.swift
+++ frontend/ios/GetMeHome/GetMeHomeApp.swift
@@ -12,38 +12,61 @@ import FirebaseCore
 
 
 class AppDelegate: NSObject, UIApplicationDelegate {
-  func application(_ application: UIApplication,
-                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-    FirebaseApp.configure()
-
-    return true
-  }
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        
+        return true
+    }
 }
 
 
 @main
 struct GetMeHomeApp: App {
-  @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
-  var body: some Scene {
-    WindowGroup {
-      NavigationStack {
-        AuthenticatedView {
-            ZStack {
-                LinearGradient(gradient: Gradient(colors: [.purple]), startPoint: .leading, endPoint: .trailing)
-                Text("GetMeHome")
-                      .fontWeight(.black)
-                      .font(.title)
-                      .foregroundStyle(.white)
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+    @StateObject private var viewModel = AuthenticationViewModel()
+    @State private var presentingLoginScreen = false
+    @State private var presentingProfileScreen = false
+    
+    
+    var body: some Scene {
+        WindowGroup {
+            NavigationStack {
+                
+                switch viewModel.authenticationState {
+                case .unauthenticated, .authenticating:
+                    VStack {
+                        ZStack {
+                            LinearGradient(gradient: Gradient(colors: [.purple]), startPoint: .leading, endPoint: .trailing)
+                            VStack {
+                                Text("GetMeHome")
+                                      .fontWeight(.black)
+                                      .font(.title)
+                                      .foregroundStyle(.white)
+                                Button("Tap here to log in") {
+                                    viewModel.reset()
+                                    presentingLoginScreen.toggle()
+                                }
+                            }
+                        }
+                        .ignoresSafeArea(edges: .all)
+                    }
+                    .sheet(isPresented: $presentingLoginScreen) {
+                        AuthenticationView()
+                            .environmentObject(viewModel)
+                    }
+                case .authenticated:
+                    VStack {
+                        ContentView()
+                        Text("You're logged in as \(viewModel.displayName).")
+                        Button("Tap here to sign out") {
+                            viewModel.signOut()
+                            //          presentingProfileScreen.toggle()
+                        }
+                    }
+                }
             }
-            .ignoresSafeArea(edges: .all)
-        
-        } content: {
-          ContentView()
-        Spacer()
-
         }
-        .ignoresSafeArea(edges: .all)
-      }
     }
-  }
 }
