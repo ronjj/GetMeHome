e4281d5431126cdf4caec322e6ec68b4384c0e92
Ronald Jabouin <ronaldjabouin2004@gmail.com>
2024-01-13 10:33:37 -0500

    tried adding support for diff email types

--- frontend/ios/GetMeHome/Models/SupportEmail.swift
+++ frontend/ios/GetMeHome/Models/SupportEmail.swift
@@ -18,7 +18,7 @@ struct SupportEmail {
         Application Name: \(Bundle.main.displayName)
         iOS: \(UIDevice.current.systemVersion)
         Device Model: \(UIDevice.current.modelName)
-        Appp Version: \(Bundle.main.appVersion)
+        App Version: \(Bundle.main.appVersion)
         App Build: \(Bundle.main.appBuild)
         \(messageHeader)
     --------------------------------------
@@ -26,9 +26,61 @@ struct SupportEmail {
     }
     
     func send(openURL: OpenURLAction) {
-        let urlString = "mailto:\(toAddress)?subject=\(subject.addingPercentEncoding(withAllowedCharacters: .urlPathAllowed) ?? "")&body=\(body.addingPercentEncoding(withAllowedCharacters: .urlPathAllowed) ?? "")"
-        guard let url = URL(string: urlString) else { return }
-        openURL(url) { accepted in
+        let gmailUrl =  "googlegmail://co?to=\(toAddress)?subject=\(subject.addingPercentEncoding(withAllowedCharacters: .urlPathAllowed) ?? "")&body=\(body.addingPercentEncoding(withAllowedCharacters: .urlPathAllowed) ?? "")"
+        let outlookUrl =  "ms-outlook://compose?to=\(toAddress)?subject=\(subject.addingPercentEncoding(withAllowedCharacters: .urlPathAllowed) ?? "")&body=\(body.addingPercentEncoding(withAllowedCharacters: .urlPathAllowed) ?? "")"
+        let yahooMail =  "ymail://mail/compose?to=\(toAddress)?subject=\(subject.addingPercentEncoding(withAllowedCharacters: .urlPathAllowed) ?? "")&body=\(body.addingPercentEncoding(withAllowedCharacters: .urlPathAllowed) ?? "")"
+        let sparkUrl =  "readdle-spark://compose?recipient=\(toAddress)?subject=\(subject.addingPercentEncoding(withAllowedCharacters: .urlPathAllowed) ?? "")&body=\(body.addingPercentEncoding(withAllowedCharacters: .urlPathAllowed) ?? "")"
+        let defaultUrl =  "mailto:\(toAddress)?subject=\(subject.addingPercentEncoding(withAllowedCharacters: .urlPathAllowed) ?? "")&body=\(body.addingPercentEncoding(withAllowedCharacters: .urlPathAllowed) ?? "")"
+        
+        
+        
+        
+        guard let gmailUrl = URL(string: gmailUrl) else {return}
+        openURL(gmailUrl) { accepted in
+            if !accepted {
+                print("""
+                This device does not support email
+                \(body)
+                """
+                )
+            }
+        }
+        
+        guard let outlookUrl = URL(string: outlookUrl) else {return}
+        openURL(outlookUrl) { accepted in
+            if !accepted {
+                print("""
+                This device does not support email
+                \(body)
+                """
+                )
+            }
+        }
+        
+        guard let yahooMail = URL(string: yahooMail) else {return}
+        openURL(yahooMail) { accepted in
+            if !accepted {
+                print("""
+                This device does not support email
+                \(body)
+                """
+                )
+            }
+        }
+        
+        guard let sparkUrl = URL(string: sparkUrl) else {return}
+        openURL(sparkUrl) { accepted in
+            if !accepted {
+                print("""
+                This device does not support email
+                \(body)
+                """
+                )
+            }
+        }
+        
+        guard let defaultUrl = URL(string: defaultUrl) else {return}
+        openURL(defaultUrl) { accepted in
             if !accepted {
                 print("""
                 This device does not support email
