e3fc3b91f55200f701eb54f46d5910d0240d0767
Ronald Jabouin <ronaldjabouin2004@gmail.com>
2024-01-17 13:27:27 -0500

    new-line for email in profile

--- frontend/ios/GetMeHome/Screens/ProfileView.swift
+++ frontend/ios/GetMeHome/Screens/ProfileView.swift
@@ -115,7 +115,7 @@ struct ProfileView: View {
                     .tint(.red)
                 }
                 Section("Contact") {
-                    Text("Have questions? Found bugs? Have a feature suggestion? Send rj336@cornell.edu an email")
+                    Text("Have questions? Found bugs? Have a feature suggestion? \nSend rj336@cornell.edu an email")
                     Button {
                         email.send(openURL: openURL)
                         AnalyticsManager.shared.logEvent(name: "ProfileView_SendEmailClicked")
