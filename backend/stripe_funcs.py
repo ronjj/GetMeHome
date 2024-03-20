b308a0521af8e706122d693cd8bc5c855e07e47d
Ronald Jabouin <rj336@cornell.edu>
2024-03-20 13:01:10 -0400

    trying to deploy to render. commented out stripe code for now

--- backend/stripe_funcs.py
+++ backend/stripe_funcs.py
@@ -1,24 +1,24 @@
 
-import stripe
-from flask import jsonify
-def payment_sheet(amount):
-  # Use an existing Customer ID if this is a returning customer
-  customer = stripe.Customer.create()
-  ephemeralKey = stripe.EphemeralKey.create(
-    customer=customer['id'],
-    stripe_version='2023-10-16',
-  )
-  paymentIntent = stripe.PaymentIntent.create(
-    amount=amount,
-    currency='usd',
-    customer=customer['id'],
-    # In the latest version of the API, specifying the `automatic_payment_methods` parameter
-    # is optional because Stripe enables its functionality by default.
-    automatic_payment_methods={
-      'enabled': True,
-    },
-  )
-  return jsonify(paymentIntent=paymentIntent.client_secret,
-                 ephemeralKey=ephemeralKey.secret,
-                 customer=customer.id,
-                 publishableKey='pk_test_TYooMQauvdEDq54NiTphI7jx')
+# import stripe
+# from flask import jsonify
+# def payment_sheet(amount):
+#   # Use an existing Customer ID if this is a returning customer
+#   customer = stripe.Customer.create()
+#   ephemeralKey = stripe.EphemeralKey.create(
+#     customer=customer['id'],
+#     stripe_version='2023-10-16',
+#   )
+#   paymentIntent = stripe.PaymentIntent.create(
+#     amount=amount,
+#     currency='usd',
+#     customer=customer['id'],
+#     # In the latest version of the API, specifying the `automatic_payment_methods` parameter
+#     # is optional because Stripe enables its functionality by default.
+#     automatic_payment_methods={
+#       'enabled': True,
+#     },
+#   )
+#   return jsonify(paymentIntent=paymentIntent.client_secret,
+#                  ephemeralKey=ephemeralKey.secret,
+#                  customer=customer.id,
+#                  publishableKey='pk_test_TYooMQauvdEDq54NiTphI7jx')
